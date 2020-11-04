1528b817bf644f14778676afac54ba4922a79859
john-craig <john.craig1@marist.edu>
2020-11-04 17:01:06 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+const path = require('path');
+//const autoprefixer = require('autoprefixer');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        chunkFilename: '[id].js',
+        publicPath: ''
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
