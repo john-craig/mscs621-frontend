b01ab62f099997d479a5bf0cf70ac7714b280f1a
john-craig <john.craig1@marist.edu>
2020-11-04 18:22:17 -0500

    Will now create a React-based page

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+
+const App = () => {
+
+  return (
+    <div>
+
+        Goodbye
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
